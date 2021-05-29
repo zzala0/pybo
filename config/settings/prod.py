160569452def0c8172acb45c5237fe69663985b4
임재하 <zzala0@gmail.com>
2021-05-29 22:11:02 +0900

    ALLOWED_HOSTS 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 
-ALLOWED_HOSTS = ['localhost', 'zzala.myds.me']
+ALLOWED_HOSTS = ['localhost', '*.zzala.myds.me']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
